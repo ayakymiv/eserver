58c64180d24ed6fde5b8885456152c95785cf1fd
Andrii Yakymiv <andriy@Andriis-MacBook-Pro.local>
2018-10-02 19:48:58 +0300

    Added handlers for the Publish and Delete events.

--- app.go
+++ app.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"github.com/gorilla/mux"
+	"io/ioutil"
 	"log"
 	"net/http"
 )
@@ -15,6 +17,7 @@ type App struct {
 func (a *App) Initialize() {
 	a.Router = mux.NewRouter()
 	a.initializeRoutes()
+	ModelsInit()
 }
 
 func (a *App) Run(addr string) {
@@ -26,28 +29,44 @@ func (a *App) initializeRoutes() {
 	a.Router.HandleFunc("/listener", a.addEvent).Methods("POST")
 	a.Router.HandleFunc("/listener/{event}", a.removeEvent).Methods("DELETE")
 	a.Router.HandleFunc("/publish/{event}", a.publishEvent).Methods("POST")
-	a.Router.PathPrefix("/").Handler(a.HandleAll)
+	//a.Router.PathPrefix("/").Handler(a.HandleAll)
 }
 
 func (a *App) addEvent(w http.ResponseWriter, r *http.Request) {
-	var e Event
+	b, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		log.Fatal("Error:", err)
+	}
+	defer r.Body.Close()
+
+	var e = Event{}
+	err = json.NewDecoder(bytes.NewReader(b)).Decode(&e)
+	if err != nil {
+		log.Fatal("Error:", err)
+	}
 	var h Handler
-	err := json.NewDecoder(r.Body).Decode(&e)
+	err = json.NewDecoder(bytes.NewReader(b)).Decode(&h)
+	e.Handlers = []Handler{h}
 	if err != nil {
 		log.Fatal("Error:", err)
 	}
-	err = json.NewDecoder(r.Body).Decode(&h)
-	if err != nil {
-		log.Fatal("Error:", err)
-	}
-	fmt.Println(w)
+	Store(e)
 }
 
 func (a *App) removeEvent(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	ModelRemove(vars["event"])
 	fmt.Println(w)
 }
 
 func (a *App) publishEvent(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	var body interface{}
+	err := json.NewDecoder(r.Body).Decode(&body)
+	if err != nil {
+		log.Fatal("Error:", err)
+	}
+	ModelPublish(vars["event"],body)
 	fmt.Println(w)
 }
 
--- models.go
+++ models.go
@@ -4,18 +4,57 @@ import (
 	"github.com/jinzhu/gorm"
 )
 
+var (
+	db, _ = gorm.Open("sqlite3", "gorm.db")
+)
+
 type Event struct {
-	gorm.Model
-	ID uint `json:”id”`
-	Name string `json:"name"`
-	Event string `json:"event"`
-	Handlers Handler
-	data interface{}
+	ID 			int			`json:"-" gorm:"primary_key";"AUTO_INCREMENT"`
+	Name 		string 		`json:"name,omitempty" gorm:"primary_key"`
+	Event 		string 		`json:"event,omitempty" sql:"size:255;unique;index"`
+	Handlers 	[]Handler	`gorm:"foreignkey:EventID"`
+	Published 	bool 		`sql:"DEFAULT:false"`
+	data 		interface{}
 }
 
 type Handler struct{
-	gorm.Model
-	ID uint `json:”id”`
-	Handler string
+	ID 			int			`json:"-" gorm:"primary_key";"AUTO_INCREMENT"`
+	EventID		int			`json:"-" gorm:"unique_key"`
+	Address 	string 		`json:"address,omitempty" sql:"unique;index"`
 
 }
+
+func ModelsInit() {
+	db.LogMode(true)
+
+	if !db.HasTable(&Event{}) {
+		db.CreateTable(&Event{},&Handler{})
+	}
+
+	db.AutoMigrate(&Event{})
+	db.AutoMigrate(&Handler{})
+}
+
+func Store(evt Event) {
+	db.Where(Event{Name: evt.Name}).Assign(evt).FirstOrCreate(&evt)
+	//if db.NewRecord(evt) {
+	//	if err := db.Create(&evt).Error; err != nil {
+	//		log.Fatal("Error:", err)
+	//	}
+	//}
+
+}
+func ModelRemove(name string) {
+	db.Where(Event{Name: name}).Association("Handlers").Delete(&Event{Name: name})
+	db.Where(Event{Name: name}).Delete(&Event{Handlers:[]Handler{}})
+}
+
+func ModelPublish(name string, data interface{}){
+	var evt = Event{}
+	db.Where(Event{Name: name}).Find(&evt)
+	evt.Published = true
+	evt.data = data
+	db.Save(&evt).Assign(evt)
+
+}
+
